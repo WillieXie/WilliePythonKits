58fc48fd98c45043007640fee1726d1cf6736b0a
willie <xieweikol@gmail.com>
2019-04-11 21:12:02 +0800

    1. make_new_old_patches_in_repo.py   a. Rename file to be `make_new_old_patches_in_repo.py`   b. Add option '-m', it is used to set manifest xml name in ".repo/manifests/" folder   c. Check manifest xml <include> node   d. When branch_name is empty, use <default> node `revision` attribute. 2. create_mirror_repo_from_local_folder.py   a. Use list instead of dictionary. So the final <project> node order in mirror manifest is the same as ori one.   b. If some project path do not exist, remove them from manifest.xml 3. Add README.md

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 # WilliePythonKits
 Use python to reduce some burden work
+
+- [Repo Kits](##RepoKits)
+
+## RepoKits
+
+- [make_new_old_patches_in_repo](###make_new_old_patches_in_repo)
+- [create_mirror_repo_from_local_folder](###create_mirror_repo_from_local_folder)
+
+### make_new_old_patches_in_repo
+
+- [DESCRIPTION](####DESCRIPTION)
+- [OPTIONS](####OPTIONS)
+- [SAMPLE](####SAMPLE)
+
+#### DESCRIPTION
+
+**Make new old patches** under [repo](https://source.android.com/source/using-repo.html) working folder.
+
+`make_new_old_patches_in_repo.py` will check manifest.xml under ``.repo/manifests/`` folder and find all projects that has the specified branch.
+
+Then iterating all matched projects and make new old patch. The output is under current ``out`` folder. And it will be compressed to zip file.
+
+#### OPTIONS
+
+    --version             show program's version number and exit
+    -h, --help            show this help message and exit
+    -s START_TIME, --start=START_TIME
+                          start time, default is today 00:00
+    -e END_TIME, --end=END_TIME
+                          end time, default is now
+    -d WORK_DIRECTORY, --directory=WORK_DIRECTORY
+                          repo base directory, default is current folder
+    -m MANIFEST_XML_NAME, --manifest=MANIFEST_XML_NAME
+                          manifest xml in ".repo/manifests/" folder, default
+                          file is "default.xml"
+    -o OEM_DIRECTORY, --oem=OEM_DIRECTORY
+                          oem directory, outside of repo base directory, default
+                          is current folder
+    -b BRANCH_NAME, --branch=BRANCH_NAME
+                          branch name to identify <project>, if empty, use
+                          <default> node revision
+    -p PROJECT_PATH, --project=PROJECT_PATH
+                          single project path, if empty, checking all projects
+
+#### SAMPLE
+
+1. Make patch for single project "/home/willie/work/idealens_version/frameworks/native" in branch "idealens", the manifest use "invision_repo.xml":
+
+   ``` bash
+   python3 make_new_old_patches_in_repo.py -s "2019-3-28 11:48:30" -d "/home/willie/work/idealens_version" -m "invision_repo.xml" -b "idealens" -p "frameworks/native"
+   ```
+
+2. Make patch for all projects in folder "/home/willie/work/idealens_version" and oem folder "/home/willie/work/idealens_oem/amss_standard_oem" whose branch is "master":
+
+   ``` bash
+   python3 make_new_old_patches_in_repo.py -s "2019-3-28 11:48:30" -d "/home/willie/work/idealens_version" -o "/home/willie/work/idealens_oem/amss_standard_oem"
+   ```
+
+## create_mirror_repo_from_local_folder
+
+- [DESCRIPTION](####DESCRIPTION)
+- [OPTIONS](####OPTIONS)
+- [SAMPLE](####SAMPLE)
+
+#### DESCRIPTION
+
+**Create Mirror repo directory** from other working repo directory.
+
+There exists one working repo directory. It has spent tons of time syncing from remote server(For example: AOSP).
+Now I want to create mirror repo directory in local server, however **DO NOT** sync from remote server(spending tons of time again).
+
+`create_mirror_repo_from_local_folder.py` can parse the downloaded working repo directory and create mirror repo directory.
+
+#### OPTIONS
+
+   --version             show program's version number and exit
+   -h, --help            show this help message and exit
+   -b BASE_FOLDER, --base=BASE_FOLDER
+                           base repo folder, default is ./base_repo
+   -d DEST_FOLDER, --dest=DEST_FOLDER
+                           dest mirror repo folder, default is ./mirror_repo
+   -r REMOTE_NAME, --remote=REMOTE_NAME
+                           remote node name in manifest.xml
+   -c CULL_PREFIX, --cull=CULL_PREFIX
+                           cull project name prefix in manifest.xml, default cull
+                           nothing
+
+
+#### SAMPLE
+
+1. Create mirror repo directory `/home/willie/work/repo_android_mirror` from working directory `/home/willie/work/android/aosp`:
+
+   ``` bash
+   python3 create_mirror_repo_from_local_folder.py -b "/home/willie/work/android/aosp" -d "/home/willie/work/repo_android_mirror"
+   ```
--- repo_kits/make_new_old_patches_in_repo.py
+++ repo_kits/make_new_old_patches_in_repo.py
@@ -3,7 +3,7 @@
 # Author: Willie
 # Version: 1.0 2019-1-15
 #    Step 1: Parse manifest.xml in .repo/manifests/invision_repo.xml, to fetch all <project> note that has attribute
-#            ``revision = "idealens"``. Then Store attribute ``path`` to list.
+#            ``revision = "`branch_name`"``. Then Store attribute ``path`` to list.
 #            This is realized in function ``parse_manifest_xml()``
 #    Step 2: The output new old folder is in current folder. Need to create ``out/new`` and ``out/old`` sub folders,
 #            which contain all matched projects.
@@ -11,13 +11,24 @@
 #    Step 3: Collect different files to new and old folder.
 #            This is realized in function ``make_new_old()``
 #
-# Sample: python3 gerrit_make_new_old_patch.py -s "2019-2-28 0:0:0" -d "/home/willie/work/idealens_version" -o "/home/willie/work/idealens_oem/amss_standard_oem" -b "idealens"
+# Sample 1: Make patch for single project "/home/willie/work/idealens_version/frameworks/native" in branch "idealens", the manifest use "invision_repo.xml"
+#
+#    python3 make_new_old_patches_in_repo.py -s "2019-3-28 11:48:30" -d "/home/willie/work/idealens_version" -m "invision_repo.xml" -b "idealens" -p "frameworks/native"
+#
+# Sample 2: Make patch for all projects in folder "/home/willie/work/idealens_version" and oem folder "/home/willie/work/idealens_oem/amss_standard_oem" whose branch is "master":
+#
+#    python3 make_new_old_patches_in_repo.py -s "2019-3-28 11:48:30" -d "/home/willie/work/idealens_version" -o "/home/willie/work/idealens_oem/amss_standard_oem"
 #
 #
 # Version: 1.1 2019-2-16 When there is no commit before the specific date in one repository, use the first commit as old commit.
 # Version: 1.2 2019-3-27 If input work directory not end with "/", add it.
 # Version: 1.3 2019-3-28 Add branch name option and single project name option.
 #                        When fetching old and new commit-id, **MUST NOT** add ``--no-merges`` option.
+# Version: 1.4 2019-4-11
+#              a. Rename file to be `make_new_old_patches_in_repo.py`
+#              b. Add option '-m', it is used to set manifest xml name in ".repo/manifests/" folder
+#              c. Check manifest xml <include> node
+#              d. When branch_name is empty, use <default> node `revision` attribute.
 
 import datetime
 import optparse
@@ -27,48 +38,107 @@ import sys
 import xml.etree.ElementTree as ET
 
 global_options = optparse.OptionParser(
-    usage="gerrit_make_new_old_patch COMMAND [ARGS]"
-    , version="%prog 1.1")
+    usage="make_new_old_patches_in_repo COMMAND [ARGS]"
+    , version="%prog 1.4")
 global_options.add_option('-s', '--start', action='store', type='string', dest='start_time', default='',
                           help='start time, default is today 00:00')
 global_options.add_option('-e', '--end', action='store', type='string', dest='end_time', default='',
                           help='end time, default is now')
 global_options.add_option('-d', '--directory', action='store', type='string', dest='work_directory', default='',
                           help='repo base directory, default is current folder')
+global_options.add_option('-m', '--manifest', action='store', type='string', dest='manifest_xml_name', default='',
+                          help='manifest xml in ".repo/manifests/" folder, default file is "default.xml"')
 global_options.add_option('-o', '--oem', action='store', type='string', dest='oem_directory', default='',
-                          help='oem directory, default is current folder')
+                          help='oem directory, outside of repo base directory, default is current folder')
 global_options.add_option('-b', '--branch', action='store', type='string', dest='branch_name', default='',
-                          help='branch name to identify <project>')
+                          help='branch name to identify <project>, if empty, use <default> node revision')
 global_options.add_option('-p', '--project', action='store', type='string', dest='project_path', default='',
-                          help='single project path, if empty,  is all projects')
+                          help='single project path, if empty, checking all projects')
 
 
-def parse_manifest_xml(file_path, branch_name):
+def is_empty(s):
     """
-    Parse .repo/manifests/manifest.xml, and find <project revision="idealens"> node\n
-    :param file_path: xml file path
-    :return: matched node 'path' attribute list, as well as <remote> node `name` attribute
+    Check input string is empty
+    :param s: string to be checked
+    :return: if empty, return true
     """
-    tree = ET.parse(file_path)
+    return (s is None) or (s == "")
+
+
+def parse_manifest_xml(out_dict, manifest_folder, manifest_name, revision_name):
+    """
+    Parse .repo/manifest.xml, and find all <project> node whose branch name is `branch_name`\n
+    If there is <include> node, check the include manifest file\n
+    :param out_dict: output dictionary. key is `project path`, value is `remote name`
+    :param manifest_folder: Input folder of `.repo/manifests/` folder
+    :param manifest_name: manifest xml name
+    :param revision_name: <project> node `revision` attribute
+    :return: <default> node `revision` attribute
+    """
+    if not manifest_folder.endswith('/'):
+        manifest_folder = manifest_folder + '/'
+    manifest_xml_path = manifest_folder + manifest_name
+
+    tree = ET.parse(manifest_xml_path)
     root = tree.getroot()
     remote_node = root.find('./remote')
     remote_name = remote_node.get('name')
-    print('start parse_manifest_xml root={} <remote> name={}'.format(root, remote_name))
+    print('\nstart parse_manifest_xml path={} remote_name={} revision_name={}'.format(manifest_xml_path, remote_name,
+                                                                                      revision_name))
 
-    path_list = []
-    for project in root.findall("./project/[@revision='{}']".format(branch_name)):
+    # Whether checking <project> 'revision' attribute base on input `branch_name`
+    allow_null_revision = False
+    default_node_revision = ''
+    if is_empty(revision_name):
+        allow_null_revision = True
+        default_node = root.find('default')
+        if default_node is None:
+            print("There is no <default> in this manifest, set default branch to be master\n")
+            default_revision_name = 'master'
+        else:
+            default_revision_name = default_node.get('revision')
+            default_node_revision = default_revision_name
+            print('input branch_name is None, use <default> revision: {}\n'.format(revision_name))
+
+    # Add all project list in <project> node whose `revision` node is matched and save to out_dict
+    # when meet two <project> nodes with same `path` attribute, the latter one will cover the former
+    project_count = 0
+    for project in root.findall("./project"):
+        revision = project.get('revision')
+        if allow_null_revision:
+            # revision can either be None or the same as <default> node.
+            accept_project = is_empty(revision) or (default_revision_name == revision)
+        else:
+            accept_project = revision_name == revision
+
+        if not accept_project:
+            continue
+
         name = project.get('name')
         path = project.get('path')
-        print('project_name={}, path={}'.format(name, path))
-        path_list.append(path)
-    return path_list, remote_name
+        # If there is no `path` attribute, set path=name
+        if is_empty(path):
+            path = name
+        print('Add project name={}, path={}'.format(name, path))
+        out_dict[path] = remote_name
+        project_count = project_count + 1
+
+    if 0 == project_count:
+        print('There is no matched project in: {}\n'.format(manifest_xml_path))
+
+    # Thirdly check include node
+    for manifest in root.findall("./include"):
+        include_xml_name = manifest.get('name')
+        parse_manifest_xml(out_dict, manifest_folder, include_xml_name, revision_name)
+
+    return default_node_revision
 
 
 def create_output_folder(base_folder_path, project_path_list):
     """
     Prepare folder for new and old files.\n
     :param base_folder_path: the path to create `out` folder
-    :param project_path_list: all project path lists
+    :param project_path_list: all project path and remote name dictionary
     :return: None
     """
     print('create_output_folder start base_folder_path={}, len(project_path_list)={}'.format(base_folder_path,
@@ -195,7 +265,7 @@ if __name__ == '__main__':
 
     # Firstly fetch parameters from input.
     start_time = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
-    if (options.start_time is None) or (options.start_time == ""):
+    if is_empty(options.start_time):
         print("Input start_time is empty set start_time={}".format(start_time))
     else:
         start_time = datetime.datetime.strptime(options.start_time, '%Y-%m-%d  %H:%M:%S')
@@ -203,7 +273,7 @@ if __name__ == '__main__':
     str_start_time = start_time.strftime("%Y-%m-%d %H:%M:%S")
 
     end_time = datetime.datetime.now()
-    if (options.end_time is None) or (options.end_time == ""):
+    if is_empty(options.end_time):
         print('Input end_time is empty set end_time={}'.format(end_time))
     else:
         end_time = datetime.datetime.strptime(options.end_time, '%Y-%m-%d  %H:%M:%S')
@@ -211,7 +281,7 @@ if __name__ == '__main__':
     str_end_time = end_time.strftime("%Y-%m-%d %H:%M:%S")
 
     repo_base_directory = os.path.dirname(os.path.realpath(__file__))
-    if (options.work_directory is None) or (options.work_directory == ""):
+    if is_empty(options.work_directory):
         print('Input work_directory is empty set repo_base_directory={}'.format(repo_base_directory))
     else:
         repo_base_directory = options.work_directory
@@ -219,8 +289,15 @@ if __name__ == '__main__':
             repo_base_directory = repo_base_directory + '/'
         print('Set repo_base_directory={}'.format(repo_base_directory))
 
-    oem_git_directory = os.path.dirname(os.path.realpath(__file__))
-    if (options.oem_directory is None) or (options.oem_directory == ""):
+    manifest_xml = 'default.xml'
+    if is_empty(options.manifest_xml_name):
+        print('Input manifest_xml_name is empty set manifest_xml={}'.format(manifest_xml))
+    else:
+        manifest_xml = options.manifest_xml_name
+        print('Set manifest_xml={}'.format(manifest_xml))
+
+    oem_git_directory = ''
+    if is_empty(options.oem_directory):
         print('Input oem_directory is empty set oem_git_directory={}'.format(oem_git_directory))
     else:
         oem_git_directory = options.oem_directory
@@ -228,42 +305,58 @@ if __name__ == '__main__':
             oem_git_directory = oem_git_directory + '/'
         print('Set oem_git_directory={}'.format(oem_git_directory))
 
-    branch_name = 'idealens'
-    if (options.branch_name is None) or (options.branch_name == ""):
+    branch_name = ''
+    if is_empty(options.branch_name):
         print('Input branch_name is empty set branch_name={}'.format(branch_name))
     else:
         branch_name = options.branch_name
         print('Set branch_name={}'.format(branch_name))
 
     single_project_path = ''
-    if (options.project_path is None) or (options.project_path == ""):
+    if is_empty(options.project_path):
         print('Input project_path is empty set single_project_path={}'.format(single_project_path))
     else:
         single_project_path = options.project_path
         print('Set single_project_path={}'.format(single_project_path))
 
-    # Secondly check exist of invision_repo_path
-    invision_repo_path = repo_base_directory + '.repo/manifests/invision_repo.xml'
-    print('invision_repo_path={}'.format(invision_repo_path))
-    if not os.path.exists(invision_repo_path):
-        print('Error {} is not valid'.format(invision_repo_path))
-        sys.exit()
+    # # Secondly check exist of invision_repo_path
+    # invision_repo_path = repo_base_directory + '.repo/manifests/invision_repo.xml'
+    # print('invision_repo_path={}'.format(invision_repo_path))
+    # if not os.path.exists(invision_repo_path):
+    #     print('Error {} is not valid'.format(invision_repo_path))
+    #     sys.exit()
 
-    # Thirdly parse manifest xml
-    project_path_list, remote_name = parse_manifest_xml(invision_repo_path, branch_name)
-    if (single_project_path != ''):
-        project_path_list.clear()
-        project_path_list.append(single_project_path)
+    # Secondly parse manifest xml
+    project_path_remote_name_dict = {}
+    manifests_folder = repo_base_directory + '.repo/manifests/'
+    default_node_name = parse_manifest_xml(project_path_remote_name_dict, manifests_folder, manifest_xml, branch_name)
+    if single_project_path != '':
+        single_project_remote_name = project_path_remote_name_dict.get(single_project_path)
+        if is_empty(single_project_remote_name):
+            print('Fatal: project: {} and branch: {} NOT Match\nExiting...'.format(single_project_path, branch_name))
+            sys.exit(1)
+
+        project_path_remote_name_dict.clear()
+        project_path_remote_name_dict[single_project_path] = single_project_remote_name
+        print('\nOnly make patch for project: {}\n'.format(single_project_path))
     else:
-        # Append oem folder.
-        project_path_list.append('oem')
+        # Try to append oem folder.
+        if not is_empty(oem_git_directory):
+            # oem_git_directory is absolute folder path
+            project_path_remote_name_dict['oem'] = 'origin'
+            print('Add project oem, path={}, remote=origin\n'.format(oem_git_directory))
 
-    # Fourthly prepare output folder
+    # if input branch_name is '', use <default> node 'revision' attribute
+    if is_empty(branch_name):
+        branch_name = default_node_name
+        print('\nupdate branch_name to be {}\n'.format(branch_name))
+
+    # Thirdly prepare output folder
     curr_working_folder_path = os.path.dirname(os.path.realpath(__file__))
     out_base_folder_path = curr_working_folder_path
-    create_output_folder(out_base_folder_path, project_path_list)
+    create_output_folder(out_base_folder_path, project_path_remote_name_dict.keys())
 
-    # Fifthly Create log file.
+    # Fourthly Create log file.
     str_time_now = datetime.datetime.now().strftime('%Y-%m-%d__%H-%M-%S')
     str_log_file = out_base_folder_path + '/out/willie_patch_log_' + str_time_now
     log_file = open(str_log_file, 'w')
@@ -272,10 +365,11 @@ if __name__ == '__main__':
     log_file.write('end_time={}\n'.format(str_end_time))
     log_file.write('repo_base_directory={}\n'.format(repo_base_directory))
     log_file.write('oem_git_directory={}\n\n'.format(oem_git_directory))
-    log_file.write('Start handling {} projects in branch idealens:\n'.format(len(project_path_list)))
+    log_file.write('Start handling {} projects in branch {}:\n'.format(len(project_path_remote_name_dict), branch_name))
 
-    # Sixthly iterate all projects and make new/old folder.
-    for idx, project_path in enumerate(project_path_list):
+    # Fifthly iterate all projects and make new/old folder.
+    idx = 0
+    for project_path, remote_name in project_path_remote_name_dict.items():
         log_file.write('{}: current project path is {}\n'.format(idx, project_path))
         git_project_path = repo_base_directory + project_path
         if 'oem' == project_path:
@@ -283,13 +377,14 @@ if __name__ == '__main__':
         # Here change back datetime.time to string
         make_new_old(git_project_path, out_base_folder_path, project_path, str_start_time, str_end_time, log_file,
                      remote_name, branch_name)
+        idx = idx + 1
 
     # Change back to original folder
     os.chdir(curr_working_folder_path)
     log_file.write('Mission complete!\n')
     log_file.close()
 
-    # Seventhly zip out folder
+    # Sixthly zip out folder
     str_zip_cmd = 'zip -r new_old_{}.zip {}'.format(str_time_now, 'out')
     os.system(str_zip_cmd)
     print('\n\nMission complete!')
